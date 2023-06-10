806b4ab31f8d2bf9c540cd353d6b3e9f6f45ddf5
--global <syoma200489@gmail.com>
2023-06-10 20:08:59 +0300

    4

--- store/admin.py
+++ store/admin.py
@@ -3,14 +3,16 @@ from django.contrib import admin
 from .models import *
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('product_name', 'in_stock', 'available', 'category')
+    list_display = ('product_name', 'slug', 'in_stock', 'available', 'category')
+    list_editable = ('product_name', 'slug', 'available')
     list_display_links = ('in_stock', 'category')
-    search_fields = ('product_name', 'description')
+    search_fields = ('product_name', 'description', 'slug')
     prepopulated_fields = {'slug': ('product_name',)}
 
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'category_name', 'slug')
+
     prepopulated_fields = {'slug': ('category_name',)}
 
 
